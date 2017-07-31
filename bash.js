8992f78e8948721d646051b3106fe72e8eb6f87d
David Berger <dgaberger@gmail.com>
2017-07-31 11:17:19 -0400

    up to cat, head, tail

--- bash.js
+++ bash.js
@@ -5,10 +5,14 @@ process.stdout.write('prompt > ');
 
 // The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', function (data) {
-  var cmd = data.toString().trim(); // remove the newline
+	var cmd = data.toString().trim(); // remove the newline
 
-  commands[cmd]()
+	cmdParsed = cmd.substring(0, cmd.indexOf(" "))
+	var arg = cmd.substring(cmd.indexOf(" ") + 1)
+	// console.log("cmd: " + cmdParsed)
+	// console.log("arg: " + arg)
 
+  commands[cmdParsed](arg)
   // process.stdout.write('You typed: ' + cmd);
   process.stdout.write('\nprompt > ');
 
--- commands.js
+++ commands.js
@@ -1,19 +1,22 @@
 var fs = require('fs');
 
 module.exports = {
-	pwd: function(){
+	pwd: function(arguments){
 		process.stdout.write(process.env.PWD)
 	},
-	date: function(){
+	date: function(arguments){
 		process.stdout.write(Date())
 	},
-	ls: function(){
+	ls: function(arguments){
 		fs.readdir('.', function(err, files) {
 		  if (err) throw err;
 		  files.forEach(function(file) {
 		    process.stdout.write(file.toString() + "\n");
 		  })
 		});
+	},
+	echo: function(arguments){
+		console.log(arguments)
 	}
 }
 
